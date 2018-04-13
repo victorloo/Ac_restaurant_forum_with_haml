e36be99f783b27d0970c5f5d30c69395612ab515
victorloo <ila9970@gmail.com>
2018-04-14 02:34:05 +0800

    add user index page with follow button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
-  before_action :find_user
+  before_action :find_user, only: [:show, :edit, :update]
+
+  def index
+    @users = User.all
+  end
 
   def show
     @commented_restaurants = @user.restaurants.uniq
