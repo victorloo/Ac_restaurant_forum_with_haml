cce373f10ad6195c15f2617a96a48c67b70f3127
victorloo <ila9970@gmail.com>
2018-04-13 19:07:23 +0800

    add user name to registration form

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,6 @@
 class Restaurant < ApplicationRecord
   mount_uploader :image, PhotoUploader
-  validates :name, presence: :true
+  validates :name, presence: true
 
   belongs_to :category, optional: true
   has_many :comments
