938232bd6329efb878e11475f60d4a6c9cff94ca
victorloo <ila9970@gmail.com>
2018-04-13 20:19:04 +0800

    user can view recent restaurants and comments on feeds page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -9,4 +9,10 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id])
     @comment = Comment.new
   end
+
+  def feeds
+    @recent_restaurants = Restaurant.all.order(created_at: :desc).limit(10)
+    @recent_comments = Comment.all.order(created_at: :desc).limit(10)
+  end
+  
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,6 @@ class Restaurant < ApplicationRecord
   mount_uploader :image, PhotoUploader
   validates :name, presence: true
 
-  belongs_to :category, optional: true
-  has_many :comments
+  belongs_to :category
+  has_many :comments, dependent: :destroy
 end
